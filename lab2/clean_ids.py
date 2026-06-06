735c531c5a281ee67c72e32b8c711959c753019d
tgschinder1 <tgschinder1@gmail.com>
2026-06-06 17:57:50 +0000

    updated script with main() wrap

--- lab2/clean_ids.py
+++ lab2/clean_ids.py
@@ -4,15 +4,27 @@ import sys
 import re
 import logging
 
-logging.basicConfig(filename="pipeline_autid.log", level=logging.INFO)
-pattern = r'^[A-Za-z0-9_-]+$'
-
-try:
-    for line in sys.stdin:
-        id = line.strip()
+def main():
+    
+    logging.basicConfig(filename="pipeline_autid.log", level=logging.INFO)
+    
+    def is_valid_youtube_id(id_str):
+        """
+        checks that id is 11 characters long and contains only allowed characters
+        """
+        pattern = r'^[A-Za-z0-9_-]+$'
         if len(id) == 11 and bool(re.match(pattern, id)) == True:
             print(id)
         else:  logging.error(f"{id} is not a valid ID")
 
-except KeyboardInterrupt:
-    pass
+    
+    try:
+        for line in sys.stdin:
+                id = line.strip()
+                is_valid_youtube_id(id)
+    
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
